3ff10b97869a14b9758a926cfd0b6220ef2bcf1f
Taylor Phillips <taylorscottphillips@gmail.com>
2020-04-10 21:26:47 +0000

    Declared variables and functions

--- main.cpp
+++ main.cpp
@@ -17,15 +17,13 @@ video: Chapter 2 - Part 3
  1) Write down the names of all of the primitives available in C++ (excluding wchar_t)
  put them here: 
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
+ Integar -- int
+ Character -- char
+ Boolean -- bool
+ Floating Point -- float
+ Double Floating Point -- double
+ Valuless -- void 
+
  
 2) for each primitive type, write out 3 variable declarations inside the variableDeclaration function.
     give each declaration an initial value
@@ -65,10 +63,34 @@ void variableDeclarations()
 {
     //example:
     int number = 2; //declaration of a primitive named 'number' with an initial value of '2'
+
+    int apples = 4;
+    int organes = 12;
+    int bannanas = 6;
     
-    
+    char smallNumberA = 1;
+    char smallNumberB = 2;
+    char smallNumberC = 3;
+
+    bool isRunning = true;
+    bool isFlying = false;
+    bool isJumping = false;
+
+    float length = 8.3467f;
+    float width = 12.3868f;
+    float height = 15.3475f;
+
+    double distance = 5.7;
+    double speed = 12.9;
+    double time = 60.2;
     
     ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
+
+    ignoreUnused(apples, organes, bannanas);
+    ignoreUnused(smallNumberA, smallNumberB, smallNumberC);
+    ignoreUnused(isRunning, isFlying, isJumping);
+    ignoreUnused(length, width, height);
+    ignoreUnused(distance, speed, time);
 }
 /*
  10 functions
@@ -83,42 +105,93 @@ bool rentACar(int rentalDuration, int carType = 0)  //function declaration with 
 /*
  1)
  */
+ float squareFeet(float length, float width)
+ {
+     ignoreUnused(length, width);
+     return {};
+ }
 
 /*
  2)
  */
+ bool playerState(bool isJumping, bool isRunning, bool isFlying)
+ {
+     ignoreUnused(isJumping, isRunning, isFlying);
+     return {};
+ }
 
 /*
  3)
  */
+ int noodleAmount(int numberOfBoxes, int noodlesPerBox)
+ {
+     ignoreUnused(numberOfBoxes, noodlesPerBox);
+     return {};
+ }
 
 /*
  4)
  */
+ bool purcahseSpeakers(int pairsOfSpeakers, float speakerPrice)
+ {
+     ignoreUnused(pairsOfSpeakers, speakerPrice);
+     return {};
+ }
 
 /*
  5)
  */
+ int itemsInDrawer(int socks, int underwear, int shirts)
+ {
+     ignoreUnused(socks, underwear, shirts);
+     return {};
+ }
 
 /*
  6)
  */
+ int fruitInStore(int apples, int oranges, int pears)
+ {
+     ignoreUnused(apples,oranges, pears);
+     return {};
+ }
 
 /*
  7)
  */
+ bool appliancesOn(bool stoveOn, bool fridgeOn, bool microwaveOn)
+ {
+     ignoreUnused(stoveOn, fridgeOn, microwaveOn);
+     return {};
+ }
 
 /*
  8)
  */
+ bool carStart(bool keyInIgnition, bool keyTurn, bool engineRunning)
+ {
+     ignoreUnused(keyInIgnition, keyTurn, engineRunning);
+     return {};
+ }
 
 /*
  9)
  */
+ float timeOfDay(bool light, bool dark)
+ {
+     ignoreUnused(light, dark);
+     return {};
+ }
 
 /*
  10)
  */
+bool flightStatus(bool boarding, bool departing, bool enRoute, bool landed)
+{
+    ignoreUnused(boarding, departing, enRoute, landed);
+    return {};
+}
+ 
 
 int main()
 {
@@ -126,27 +199,48 @@ int main()
     auto carRented = rentACar(6, 2); 
     
     //1)
+    auto roomSize = squareFeet(8.5f, 12.0f);
     
     //2)
+    auto playerRunning = playerState(false, true, false);
     
     //3)
+    auto totalNoodles = noodleAmount(3, 100);
     
     //4)
+    auto speakersBought = purcahseSpeakers(1, 499.99f);
     
     //5)
+    auto totalDrawerItems = itemsInDrawer(4, 6, 2);
     
     //6)
+    auto totalFruit = fruitInStore(56, 45, 21);
     
     //7)
+    auto allAppliancesOn = appliancesOn(true, true, true);
     
     //8)
+    auto isCarOn = carStart( true, true, true);
     
     //9)
+    auto isAM = timeOfDay(true, false);
     
     //10)
+    auto checkFlightStatus = flightStatus(false, true, false, false);
     
     
     ignoreUnused(carRented);
+    ignoreUnused(roomSize);
+    ignoreUnused(playerRunning);
+    ignoreUnused(totalNoodles);
+    ignoreUnused(speakersBought);
+    ignoreUnused(totalDrawerItems);
+    ignoreUnused(totalFruit);
+    ignoreUnused(allAppliancesOn);
+    ignoreUnused(isCarOn);
+    ignoreUnused(isAM);
+    ignoreUnused(checkFlightStatus);
+
     std::cout << "good to go!" << std::endl;
     return 0;    
 }
