a78bf13c814dbb5fef0544b9b0b4f3b3ab2b640e
DTekieli <73246957+DTekieli@users.noreply.github.com>
2022-04-05 18:55:30 +0000

    finished tasks. Added comments behind errors that I don't understand

--- main.cpp
+++ main.cpp
@@ -15,19 +15,18 @@ video: Chapter 2 - Part 3
  
  
  1) Write down the names of the 6 major primitive types available in C++  here:
+"Primitive Data Types: These data types are built-in or predefined data types and can be used directly by the user to declare variables."
  
- 
- 
- 
- 
- 
- 
- 
- 
+1.    int     = integer (Ganzzahl)                -> Integers typically requires 4 bytes of memory space and ranges from -2147483648 to 2147483647. 
+2.    char    = character (Buchstabe)             -> can take 256 values which corresponds to entries in the ASCII table. typically requires 1 byte of memory space and ranges from -128 to 127 or 0 to 255. 
+3.    bool    = boolean (true/false)
+4.    float   = floating point (Fließkommazahl)
+5.    double  = double floating point             -> floating point value requires 8 byte (64bit) and therefore double memory space. It is "double precise")
+6.    void    = declares functions or methods with not-returning values / procedures. " When used as a function return type, the void keyword specifies that the function doesn't return a value. When used for a function's parameter list, void specifies that the function takes no parameters. When used in the declaration of a pointer, void specifies that the pointer is "universal."
  
 2) for each primitive type, write out 3 variable declarations inside the variableDeclaration() function on line 59.
     a) give each variable declaration an initial value
-        - just ignore wchar_t. you do not need to declare 3 variables of type 'wchar_t'
+        - just ignore wchar_t. you do not need to declare 3 variables of type 'wchar_t' [Wide Character -> "wide char can take on 65536 values which corresponds to UNICODE values which is            a recent international standard which allows for the encoding of characters for virtually all languages and commonly used symbols."]
         - 'void' is a return type. you do not need to declare 3 variables of type 'void'.
     b) at the end of the function, call ignoreUnused once and pass all of your variables to it. see line 71 for an example
  
@@ -64,10 +63,26 @@ void variableDeclarations()
 {
     //example:
     int number = 2; //declaration of a variable named "number", that uses the primitive type 'int', and the variable's initial value is '2'
-    
+    int number2 = 4;
+    int number3 = 1;
 
+    char first = 'a';
+    char second = 'b';
+    char third = 'c';
+
+    bool hungry = false;
+    bool thirsty = false;
+    bool teaIsOnTheTable = true;
     
-    ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
+    float size = 1.77f;
+    float temperature = 19.2f;
+    float pressure = 180.1f;
+
+    double speedOfSound = 343.0001;
+    double weightOfH2O = 1.00784;
+    double tempOfSun = 5500.00000001;
+   
+    ignoreUnused(number, number2, number3, first, second, third, hungry, thirsty, teaIsOnTheTable, size, temperature, pressure, speedOfSound, weightOfH2O, tempOfSun); //passing each variable declared to the ignoreUnused() function
 }
 
 /*
@@ -82,44 +97,90 @@ bool rentACar(int rentalDuration, int carType = 0)  //function declaration with 
 } 
 
 /*
- 1)
+ 1) 
  */
-
+bool goSleep (int startTime, int wakeTime)
+{
+    ignoreUnused(startTime = 23, wakeTime = 6);
+    return {};
+}
 /*
  2)
  */
-
+void runFan (bool state)
+{
+    ignoreUnused(state);
+}
 /*
  3)
  */
-
+float inAir (char pressure, double location, bool full)
+{
+    ignoreUnused(pressure, location, full);
+    return{};
+}
 /*
  4)
  */
+int buildingsInTown (char name, double location, int count)
+{
+    ignoreUnused(name, location, count);
+    return{};
+}
 
 /*
  5)
  */
+int cleaningKitchen (bool floor, bool clean, int garbage)
+{
+    ignoreUnused(floor, clean, garbage);
+    return{};
+}
+
 
 /*
  6)
  */
 
+void goForWalk (int distance = 3000, bool day = true) // WHen exactly do I need to set a default value? 
+{
+    ignoreUnused(distance, day);
+}
+
 /*
  7)
  */
+int playMusic (char trackName, int volume = 10, bool goodMusicTaste = false)
+{
+    ignoreUnused(trackName, volume, goodMusicTaste);
+    return{};
+}
 
 /*
  8)
  */
+int carveInStone (char fontType, double laserTime, int stoneType)
+{
+    ignoreUnused(fontType, laserTime, stoneType);
+    return{};
+}
 
 /*
  9)
  */
+bool moveMarble (int position = 1, int marbleID = 0)
+{
+    ignoreUnused(position, marbleID);
+    return{};
+}
 
 /*
  10)
  */
+void makeCoffee (int beanType, int grindTime, char brand)
+{
+    ignoreUnused(beanType, grindTime, brand);
+}
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
@@ -141,24 +202,34 @@ int main()
     auto carRented = rentACar(6, 2); 
     
     //1)
+    auto sleepAtNight = goSleep(23, 6);
     
     //2)
-    
+    auto tooHot = runFan(true); // Why is type void in this case not correct?
+        
     //3)
+    auto fly = inAir(99, 91.1850982748, true);
     
     //4)
+    auto countBuildingsInHamburg = buildingsInTown('v', 123345456.23473458745687, 875690); // I chose "char name" earlier to being able to put in a word instead of a letter. What did I get wrong here?
     
     //5)
+    auto cleaningDay = cleaningKitchen(true, true, 4);
     
     //6)
+    auto movementAction = goForWalk(1000, true); // Why is void not the right type here? I chose it, beacause I do not expect an output return value. Same @10)
     
     //7)
+    auto homeAlona = playMusic('a', 15, true); // same question @4
     
     //8)
+    auto stoneDesign = carveInStone('h', 3, 59);
     
     //9)
+    auto playMarbles = moveMarble(1, 1);
     
     //10)
+    auto morningAction = makeCoffee(1, 2, 'a');
     
     
     ignoreUnused(carRented);
